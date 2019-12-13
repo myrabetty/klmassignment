73b8c96e5c871b326c2654059d3e7c68f3f96549
elisabetta.semboloni <elisabetta.semboloni@bestseller.com>
2019-12-13 14:27:53 +0100

    let's separate the two at least

--- src/main/java/com/afkl/travel/exercise/controller/ApplicationController.java
+++ src/main/java/com/afkl/travel/exercise/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package com.afkl.travel.exercise.controller;
 
-import com.afkl.travel.exercise.client.ClientApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -18,13 +16,10 @@ import java.util.Optional;
 public class ApplicationController {
 
     private LocationService locationService;
-    private ClientApi clientApi;
 
     @Autowired
-    public ApplicationController(LocationService locationService, ClientApi clientApi) {
+    public ApplicationController(LocationService locationService) {
         this.locationService = locationService;
-        this.clientApi = clientApi;
-
     }
 
     @RequestMapping(value = "/locations",
@@ -40,11 +35,5 @@ public class ApplicationController {
     public Optional<UserLocation> findByTypeAndCode(Locale locale, @PathVariable("type") String type, @PathVariable("code") String code) {
         return locationService.findByTypeAndCode(locale.getLanguage(), type, code);
     }
-
-    @RequestMapping(value="/trigger")
-    public void triggerClient() throws IOException {
-        clientApi.executeGetRequest();
-    }
-
 
 }
